2521c807195e3340998dff19def2e8438dafcd60
Chetanchaudhary11 <cc181972@gmail.com>
2024-01-09 11:14:03 +0530

    Sqf Assesment

---
+++ Blood-Hub/lib/screens/details/components/body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mobileapp/models/personlist.dart';
+import 'product_description.dart';
+import 'top_rounded_container.dart';
+import 'product_images.dart';
+
+class Body extends StatelessWidget {
+  final Person product;
+
+  const Body({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ProductImages(product: product),
+        TopRoundedContainer(
+          color: Colors.white,
+          child: Column(
+            children: [
+              ProductDescription(
+                product: product,
+                pressOnSeeMore: () {},
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
